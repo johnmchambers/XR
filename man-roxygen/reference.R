d79a1f3e431b2e08240d730764331053568aa4b0
John M. Chambers <jmc@stat.stanford.edu>
2016-08-21 09:29:41 -0700

    ref to pdf of chapter

--- man-roxygen/reference.R
+++ man-roxygen/reference.R
@@ -2,4 +2,4 @@
 #' Chambers, John M. (2016)
 #' \emph{Extending R},
 #' Chapman & Hall/CRC.
-#' (Chapter 12 is included in the \code{"doc"} directory of this package.)
+#' ( Chapter 12, discussing this package, is included in the package: \url{../doc/Chapter_XR.pdf}.)
